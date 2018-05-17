ba42c3d96dccd92a394c274393a1aee59223bc82
Eric Lippmann <eric.lippmann@netways.de>
2018-05-17 11:38:05 +0200

    Add doc to HtmlElement

--- src/HtmlElement.php
+++ src/HtmlElement.php
@@ -2,14 +2,19 @@
 
 namespace ipl\Html;
 
+/**
+ * The HtmlElement represents any HTML element
+ *
+ * A typical HTML element includes a tag, attributes and content.
+ */
 class HtmlElement extends BaseHtmlElement
 {
     /**
-     * Container constructor.
+     * Create a new HTML element from the given tag, attributes and content
      *
-     * @param string $tag
-     * @param Attributes|array $attributes
-     * @param ValidHtml|array|string $content
+     * @param   string                  $tag        The tag for the element
+     * @param   Attributes|array        $attributes The HTML attributes for the element
+     * @param   ValidHtml|string|array  $content    The content of the element
      */
     public function __construct($tag, $attributes = null, $content = null)
     {
