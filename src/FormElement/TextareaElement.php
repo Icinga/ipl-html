aa01164b461c070118b02319540905b9b214bf1a
Thomas Gelf <thomas@gelf.net>
2018-12-25 19:55:31 +0100

    TextArea: show configured value
    fixes #7

--- src/FormElement/TextareaElement.php
+++ src/FormElement/TextareaElement.php
@@ -5,4 +5,21 @@ namespace ipl\Html\FormElement;
 class TextareaElement extends BaseFormElement
 {
     protected $tag = 'textarea';
+
+    public function setValue($value)
+    {
+        parent::setValue($value);
+        $this->setContent($value);
+
+        return $this;
+    }
+
+    protected function registerValueCallback()
+    {
+        $this->getAttributes()->registerAttributeCallback(
+            'value',
+            null,
+            [$this, 'setValue']
+        );
+    }
 }
