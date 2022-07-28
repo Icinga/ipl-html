2ae7979cbdbfd3a371dc33bef4ddd6c0009129c0
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-07-28 15:52:25 +0200

    MultiselectElement: Remove obsolate methods and minor fixes
    * Add multiple attribute in `$defaultAttrributes` and remove constructor
    * Don't validate in `setValue()`, the `validate()` method is resposible for this.

--- src/FormElement/MultiselectElement.php
+++ src/FormElement/MultiselectElement.php
@@ -6,13 +6,10 @@ use ipl\Html\Attributes;
 
 class MultiselectElement extends SelectElement
 {
+    /** @var array Selected values */
     protected $value = [];
 
-    public function __construct($name, $attributes = null)
-    {
-        parent::__construct($name, $attributes);
-        $this->getAttributes()->add('multiple', true);
-    }
+    protected $defaultAttributes = ['multiple' => true];
 
     protected function registerValueCallback(Attributes $attributes)
     {
@@ -30,56 +27,10 @@ class MultiselectElement extends SelectElement
 
     public function setValue($value)
     {
-        if (empty($value)) { // null, '', []
-            $values = [];
-        } else {
-            $values = (array) $value;
-        }
-        $invalid = [];
-        foreach ($values as $val) {
-            if ($option = $this->getOption($val)) {
-                if ($option->getAttributes()->has('disabled')) {
-                    $invalid[] = $val;
-                }
-            } else {
-                $invalid[] = $val;
-            }
-        }
-        if (count($invalid) > 0) {
-            $this->failForValues($invalid);
-            return $this;
-        }
-
-        $this->value = $values;
+        $this->value = empty($value) ? [] : (array) $value;
         $this->valid = null;
-        $this->updateSelection();
 
         return $this;
-    }
-
-    protected function failForValues($values)
-    {
-        $this->valid = false;
-        if (count($values) === 1) {
-            $value = array_shift($values);
-            $this->addMessage("'$value' is not allowed here");
-        } else {
-            $valueString = implode("', '", $values);
-            $this->addMessage("'$valueString' are not allowed here");
-        }
-    }
-
-    public function isValid()
-    {
-        if ($this->valid === null) {
-            if ($this->isRequired() && empty($this->getValue())) {
-                return false;
-            }
-
-            $this->validate();
-        }
-
-        return $this->valid;
     }
 
     public function validate()
@@ -97,25 +48,5 @@ class MultiselectElement extends SelectElement
         BaseFormElement::validate();
 
         return $this;
-    }
-
-    public function updateSelection()
-    {
-        foreach ($this->options as $value => $option) {
-            if (in_array($value, $this->value)) {
-                $option->getAttributes()->add('selected', true);
-            } else {
-                $option->getAttributes()->remove('selected');
-            }
-        }
-
-        return $this;
-    }
-
-    protected function assemble()
-    {
-        foreach ($this->options as $option) {
-            $this->add($option);
-        }
     }
 }
