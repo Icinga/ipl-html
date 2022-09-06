541c341e1cf7f1ffc4e44e37579d23a06c4e43c6
Timm Ortloff <timm.ortloff@icinga.com>
2022-09-06 13:53:32 +0200

    Test FieldsetElement
    Co-authored-by: Eric Lippmann <eric.lippmann@icinga.com>

---
+++ tests/FormElement/FieldsetElementTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace ipl\Tests\Html\FormElement;
+
+use ipl\Html\Form;
+use ipl\Html\FormElement\FieldsetElement;
+use ipl\Tests\Html\TestCase;
+use LogicException;
+
+class FieldsetElementTest extends TestCase
+{
+    private const SELECT_OPTIONS_TO_TEST = [
+        null  => 'Nothing',
+        1     => 'One',
+        'two' => 2,
+    ];
+
+    public function testElementLoading(): void
+    {
+        $form = (new Form())
+            ->addElement('fieldset', 'test_fieldset');
+
+        $this->assertInstanceOf(FieldsetElement::class, $form->getElement('test_fieldset'));
+    }
+
+    public function testGetValueWithDefaultWithoutNameThrowsException()
+    {
+        $this->expectException(LogicException::class);
+
+        (new FieldsetElement('test_fieldset'))->getValue(null, 'default');
+    }
+
+    public function testOneDimension(): void
+    {
+        $fieldset = (new FieldsetElement('test_fieldset'))
+            ->addElement('select', 'test_select', ['options' => self::SELECT_OPTIONS_TO_TEST])
+            ->populate(['test_select' => 1]);
+
+        $expected = <<<'HTML'
+<fieldset name="test_fieldset">
+  <select name="test_fieldset[test_select]">
+    <option value="">Nothing</option>
+    <option selected="selected" value="1">One</option>
+    <option value="two">2</option>
+  </select>
+</fieldset>
+HTML;
+
+        $this->assertHtml($expected, $fieldset);
+    }
+
+    public function testTwoDimensions(): void
+    {
+        $inner = (new FieldsetElement('inner'))
+            ->addElement('select', 'test_select', ['options' => self::SELECT_OPTIONS_TO_TEST]);
+        $outer = (new FieldsetElement('outer'))
+            ->addElement($inner)
+            ->populate([
+                'inner' => ['test_select' => 'two']
+            ]);
+
+        $expected = <<<'HTML'
+<fieldset name="outer">
+  <fieldset name="outer[inner]">
+    <select name="outer[inner][test_select]">
+      <option value="">Nothing</option>
+      <option value="1">One</option>
+      <option selected="selected" value="two">2</option>
+    </select>
+  </fieldset>
+</fieldset>
+HTML;
+
+        $this->assertHtml($expected, $outer);
+    }
+
+    public function testMultipleDimensions(): void
+    {
+        $outer = new FieldsetElement('outer');
+
+        foreach (static::SELECT_OPTIONS_TO_TEST as $key => $value) {
+            $inner = (new FieldsetElement($key))
+                ->addElement('select', 'test_select', [
+                    'value'   => $value,
+                    'options' => self::SELECT_OPTIONS_TO_TEST
+                ]);
+            $outer->addElement($inner);
+        }
+
+        $expected = <<<'HTML'
+<fieldset name="outer">
+  <fieldset name="outer[]">
+    <select name="outer[][test_select]">
+      <option value="">Nothing</option>
+      <option value="1">One</option>
+      <option value="two">2</option>
+    </select>
+  </fieldset>
+  <fieldset name="outer[1]">
+    <select name="outer[1][test_select]">
+      <option value="">Nothing</option>
+      <option value="1">One</option>
+      <option value="two">2</option>
+    </select>
+  </fieldset>
+  <fieldset name="outer[two]">
+    <select name="outer[two][test_select]">
+      <option value="">Nothing</option>
+      <option value="1">One</option>
+      <option value="two">2</option>
+    </select>
+  </fieldset>
+</fieldset>
+HTML;
+
+        $this->assertHtml($expected, $outer);
+    }
+
+    public function testOriginalElementNames(): void
+    {
+        $inner = (new FieldsetElement('inner'))
+            ->addElement('select', 'test_select');
+        $outer = (new FieldsetElement('outer'))
+            ->addElement($inner);
+
+        $this->assertSame(
+            'test_select',
+            $outer->getElement('inner')->getElement('test_select')->getName()
+        );
+    }
+}
