aa344b8d07a586dccb75773f7486172734c9f6a4
Timm Ortloff <timm.ortloff@icinga.com>
2022-10-25 09:54:29 +0200

    Add Form test to check whether the Prefix Button is only set once, even if rendering multiple times.

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -52,4 +52,28 @@ HTML;
 
         $this->assertHtml($expected, $this->form);
     }
+
+    public function testPrefixSubmitButtonAddedOnlyOnce(): void
+    {
+        $submitButton = $this->form->createElement('submit', 'submitCreate');
+        $this->form->registerElement($submitButton);
+
+        $this->form->addElement('submit', 'submitDelete');
+        $this->form->addHtml($submitButton);
+
+        $this->form->setSubmitButton($submitButton);
+
+        $expected = <<<'HTML'
+    <form method="POST">
+      <input name="submit_pre" style="border: 0;height: 0;margin: 0;padding: 0;visibility: hidden;width: 0;position: absolute" type="submit" value="submit_pre"/>
+      <input name="submitDelete" type="submit" value="submitDelete"/>
+      <input name="submitCreate" type="submit" value="submitCreate"/>
+    </form>
+HTML;
+
+        $this->form->render();
+        $this->form->render();
+
+        $this->assertHtml($expected, $this->form);
+    }
 }
