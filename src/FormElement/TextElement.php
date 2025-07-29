1509f38346995626d24a38c4fc11f7a54f490d9b
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2025-07-29 12:23:18 +0200

    TextElement: Add support for `placeholder` attribute

--- src/FormElement/TextElement.php
+++ src/FormElement/TextElement.php
@@ -2,7 +2,43 @@
 
 namespace ipl\Html\FormElement;
 
+use ipl\Html\Attributes;
+
 class TextElement extends InputElement
 {
     protected $type = 'text';
+
+    /** @var ?string Placeholder text for the input */
+    protected ?string $placeholder = null;
+
+    /**
+     * Get the placeholder
+     *
+     * @return ?string
+     */
+    public function getPlaceholder(): ?string
+    {
+        return $this->placeholder;
+    }
+
+    /**
+     * Set the placeholder
+     *
+     * @param ?string $placeholder
+     *
+     * @return $this
+     */
+    public function setPlaceholder(?string $placeholder): self
+    {
+        $this->placeholder = $placeholder;
+
+        return $this;
+    }
+
+    protected function registerAttributeCallbacks(Attributes $attributes): void
+    {
+        parent::registerAttributeCallbacks($attributes);
+
+        $attributes->registerAttributeCallback('placeholder', [$this, 'getPlaceholder'], [$this, 'setPlaceholder']);
+    }
 }
